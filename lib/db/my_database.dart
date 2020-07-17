1cac06662a5bd9725cb7e5189d171eb7d344da6d
Gustavo Souza <gustavo10ssouza@gmail.com>
2020-07-16 21:02:59 -0300

    update

--- lib/db/my_database.dart
+++ lib/db/my_database.dart
@@ -1,12 +1,10 @@
 import 'package:moor_flutter/moor_flutter.dart';
 
-part 'my_database.g.dart'; ///notação que diz que vai gerar um arquivo para ser utilizado
-//Utilizo classes e objetos para gerar essa tabela com o moor;
+part 'my_database.g.dart';
 
 class Todos extends Table {
-//Criar colunas através dos elementos que ja vem dentro do flutter
-  IntColumn get id => integer().autoIncrement()(); //Já executo o método com esse ();
-  TextColumn get description => text().withLength(min: 3, max: 50)(); //min 3 caracters, max 50 caracters
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get description => text().withLength(min: 3, max: 50)(); 
   BoolColumn get completed => boolean()();
 }
 
@@ -15,16 +13,15 @@ class Favorites extends Table {
   IntColumn get idTodo => integer()();
 }
 
-//Base de dados
-@UseMoor(tables: [Todos, Favorites]) ///Usa uma anotação para dizer que se trata da uma base de dados
+@UseMoor(tables: [Todos, Favorites]) 
 class MyDatabase extends _$MyDatabase {
 
-  static final MyDatabase instance   = MyDatabase._internal(); //Instância única do BD
-  MyDatabase._internal() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite')); //Iniciando a base de dados, to acessando o construtor da classe MyDatabase
+  static final MyDatabase instance   = MyDatabase._internal(); 
+  MyDatabase._internal() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite')); 
 
-  //Métodos para acessar meus dados dentro da minha tabela
+ 
 
-  Stream<List<Todo>> getAllTodos() { //Stream é reativo, toda vez que a base de dados for alterada, ele altera automaticamente
+  Stream<List<Todo>> getAllTodos() { 
     return select(todos).watch();
   }
 
@@ -33,14 +30,12 @@ class MyDatabase extends _$MyDatabase {
   }
 
   Future deleteTodo(id) {
-    //Quero deletar a tabela todos na colunatodo, quando id dele for igual ao id passado por parametro, ele apaga
+  
     var query = delete(todos);
     query.where((todo) => todo.id.equals(id));
     return query.go();
   }
 
   @override
-  // TODO: implement schemaVersion
-  // Version banco de dados
   int get schemaVersion => 1;
 }
